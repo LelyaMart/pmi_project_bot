390db718e4e06b8d8d6df97e55e1b1330cbd8366
LelyaMart <91188250+LelyaMart@users.noreply.github.com>
2021-10-29 12:16:25 -0400

    Update classification.py

--- classification.py
+++ classification.py
@@ -30,17 +30,10 @@ def face_Descriptor(img):
     sp = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
     facerec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
     detector = dlib.get_frontal_face_detector()
-    
-    win1 = dlib.image_window()
-    win1.clear_overlay()
-    win1.set_image(img)
 
     dets = detector(img, 1)
     for k, d in enumerate(dets):
         shape = sp(img, d)
-        win1.clear_overlay()
-        win1.add_overlay(d)
-        win1.add_overlay(shape)
 
     face_descriptor1 = facerec.compute_face_descriptor(img, shape)
     return face_descriptor1
