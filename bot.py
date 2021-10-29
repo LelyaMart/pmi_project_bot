ebe78653ed8a01999575afd24ebe1d9831d05965
LelyaMart <91188250+LelyaMart@users.noreply.github.com>
2021-10-29 12:25:42 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ def getPhoto(message):
         imag = requests.get(x[1]).content
         imag = Image.open(BytesIO(imag))
         bot.send_photo(message.from_user.id, imag, caption = x[2])
-    except Exception as e:
-        bot.send_message(message.from_user.id, str(e))
+    except:
+        bot.send_message(message.from_user.id, "Ой, я не нашёл твоё лицо( Отправь другую фотографию")
 
 bot.polling(none_stop=True, interval=0)
